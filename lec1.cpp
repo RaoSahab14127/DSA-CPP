0f7c6a3d4a3f0d98eea3484a725184bcd9d7355a
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-09-01 22:09:09 +0500

    learn variable in cpp

---
+++ lec1.cpp
@@ -0,0 +1,20 @@
+// print
+#include <iostream>
+using namespace std;
+int main() {
+    
+cout<< "Hammad" <<endl;
+    
+}
+// Variables
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10;
+char b = 'v';
+bool c = true;
+float d = 1.2;
+double e = 1.23;
+cout << sizeof(e) <<endl;
+    
+}
