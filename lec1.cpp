cc4bbd5419e3867f4529d8f3c40404e7b1029417
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-09-02 00:17:40 +0500

    logical operator done

--- lec1.cpp
+++ lec1.cpp
@@ -18,3 +18,14 @@ double e = 1.23;
 cout << sizeof(e) <<endl;
     
 }
+// Unsigned and Operators
+#include <iostream>
+using namespace std;
+int main() {
+unsigned int a = 'a';
+int b = 2;
+int c =  a+b;
+
+cout << c<a  <<endl;
+    
+}
