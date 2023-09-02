6acaca5dc84ea36de943f59dab6959d796eddfed
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-09-02 14:22:13 +0500

    input and ascii

--- lec2.cpp
+++ lec2.cpp
@@ -17,3 +17,24 @@ int main() {
         cout<< "You are child" <<endl;
     }
 }
+// detect input is small letter capital or numerical
+#include <iostream>
+using namespace std;
+int main() {
+    char x;
+    cout<<"Enter Your char...:" <<endl;
+    cin >>x;
+    int res = int(x);
+    if(res>=48 && x<=57){
+        cout<< "Enter char is numeric"<<endl;
+    }
+    else if (res>=97 && x<=122){
+        cout<< "Enter char is small letter"<<endl;
+    }
+    else if(res>=65 && x<=90){
+        cout<< "Enter char is capital letter"<<endl;
+    }
+    else{
+        cout<< "others char"<<endl;
+    }
+}
