bd3f7591ecab316c04a20e3a95605338d338046b
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-09-03 21:32:37 +0500

    switch and functions

---
+++ lec3.cpp
@@ -0,0 +1,16 @@
+// switch and function
+#include <iostream>
+using namespace std;
+int age_days(int a){
+    int res = a * 365;
+    return res;
+}
+int main() {
+    int age ;
+    cin>>age;
+    int day = age_days(age);
+    switch(day){
+        case (10): cout<<"10"<<endl;
+        default: cout <<"your age is " << day <<endl;
+    }
+}
