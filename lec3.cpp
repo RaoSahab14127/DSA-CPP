74627369dffbadbc01a073e95e1c0f400f9bd5a1
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-09-17 22:33:33 +0500

    swap alternate values

--- lec3.cpp
+++ lec3.cpp
@@ -14,3 +14,33 @@ int main() {
         default: cout <<"your age is " << day <<endl;
     }
 }
+
+
+// swape alernate values
+#include <iostream>
+using namespace std;
+void print(int arr[], int size){
+    for(int i =0; i<size; i+=1){
+        cout<<arr[i];
+    }
+        cout<<endl;
+}
+void swapi(int arr[], int size){
+    for(int i =0; i<size; i+=2){
+        if (i+1 < size){
+        swap(arr[i],arr[i+1] );
+            
+        }
+    }
+}
+int main(){
+    cout<<"startingg..."<<endl;
+    int even[10]= {1,2,3,4,5,6};
+    int odd[10]= {1,2,3,4,5};
+    
+    swapi(even, 6);
+    print(even, 6);
+    swapi(odd, 5);
+    print(odd, 5);
+    
+}
